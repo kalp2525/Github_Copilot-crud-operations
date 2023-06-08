1adf9b0f5cd7bcce1e9d52c58570741ad5d57ac2
munnokd <kalp@dasinfomedia.com>
2023-06-08 13:51:49 +0530

    Project initialized

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+// write code for basic crud operation in express js
+
+// 1. create a server
+const express = require('express');
+require('dotenv').config()
+// import mongoose module
+const mongoose = require('mongoose');
+// import routers module
+const routers = require('./routers/crud');
+
+const app = express();
+
+// connect to mongodb
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connect to MongoDB...', err));
+
+app.use(express.json());
+
+// 2. create a route
+app.get('/', (req, res) => {
+    res.send('Welcome to the home page.');
+}
+);
+
+// user routers here
+app.use('/crud', routers);
+
+// 3. listen to the port
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+}
+);
